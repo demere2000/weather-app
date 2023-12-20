2f61a497d85a30bd2ddf8e69ddf299de51e6d043
Demere Walker <blessedtrulywalker32@gmail.com>
2023-12-19 20:16:21 -0600

    Updated Weather.js to allow for user selection of city

--- src/Weather.js
+++ src/Weather.js
@@ -1,60 +1,53 @@
-// Importing React and its useState hook
-import React, { useState } from 'react';
+import React, { useState } from 'react'; // Import React and useState hook
 
-// Defining the Weather component
 function Weather() {
-  // Creating a state variable 'weatherData' to store the weather information
-  // 'setWeatherData' is the function to update this state
-  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState(''); // State for storing user input city
+  const [weatherData, setWeatherData] = useState(null); // State for storing weather data
 
-  // Defining an asynchronous function to fetch weather data
+  // Function to update the city state as the user types
+  const handleInputChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  // Function to fetch weather data from OpenWeatherMap API
   const fetchWeather = async () => {
-    // Your Weatherstack API key (replace 'YOUR_API_KEY' with your actual key)
-    const apiKey = '4bcd8591931f97215d7b45892b6d0133';
-    // The city for which you want to fetch the weather (replace 'YourCity')
-    const city = 'London';
-    // The URL for the Weatherstack API, including the API key and the city query
+    if (!city) return; // If no city is entered, exit the function
+    const apiKey = '4bcd8591931f97215d7b45892b6d0133'; // Replace with your API key
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-
     try {
-      // Making an API request to the provided URL
-      const response = await fetch(url);
-      // Parsing the JSON response from the API
-      const data = await response.json();
-      // Logging the data for inspection
-      console.log(data);
-      // Updating the 'weatherData' state with the fetched data
-      setWeatherData(data);
+      const response = await fetch(url); // Fetching weather data from API
+      const data = await response.json(); // Parsing the JSON response
+      if (data.cod !== 200) {
+        throw new Error(data.message); // Throw an error if response is not successful
+      }
+      setWeatherData(data); // Update the weatherData state with the fetched data
     } catch (error) {
-      // Logging any errors that occur during the fetch operation
       console.error('Error fetching weather data:', error);
-      // Resetting the 'weatherData' state in case of an error
-      setWeatherData(null);
+      setWeatherData(null); // Reset weather data on error
     }
   };
 
-  // The return statement of the Weather component, which describes its rendered UI
+  // Render the weather component UI
   return (
     <div>
-      {/* A button that, when clicked, calls the 'fetchWeather' function */}
-      <button onClick={fetchWeather}>Get Weather</button>
-      {/* Conditional rendering to check if 'weatherData', 'location', and 'current' exist */}
-      {weatherData && weatherData.location && weatherData.current && (
+      <input // Input field for entering city name
+        type="text"
+        value={city}
+        onChange={handleInputChange}
+        placeholder="Enter city"
+      />
+      <button onClick={fetchWeather}>Get Weather</button> // Button to trigger weather data fetch
+      {weatherData && ( // Conditional rendering of weather data
         <div>
-          {/* Displaying the city name */}
-          <h2>Weather in {weatherData.location.name}</h2>
-          {/* Displaying the temperature */}
-          <p>Temperature: {weatherData.current.temperature} °C</p>
-          {/* Displaying the first weather description */}
-          <p>Weather: {weatherData.current.weather_descriptions[0]}</p>
-          {/* Displaying the humidity */}
-          <p>Humidity: {weatherData.current.humidity}%</p>
+          <h2>Weather in {weatherData.name}</h2>
+          <p>Temperature: {weatherData.main.temp} °C</p>
+          <p>Weather: {weatherData.weather[0].main}</p>
+          <p>Humidity: {weatherData.main.humidity}%</p>
         </div>
       )}
     </div>
   );
 }
 
-// Exporting the Weather component to be used in other parts of the app
-export default Weather;
+export default Weather; // Export the Weather component
